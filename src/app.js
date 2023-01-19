a0bef76166c84ff3f096b9c07daead5b483ac015
Balu <24481004+undefined@users.noreply.github.com>
2023-01-19 01:02:02 +0000

    Push from showhide

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ import { LightningElement } from "lwc";
 
 export default class App extends LightningElement {
   title = "Download Brand Images";
-
   showFeatures = true;
+  downloadZip = false;
+  hideUpload = false;
+  showhide(event) {
+        this.downloadZip = true;
+        this.hideUpload = true;
+    }
 
 }
